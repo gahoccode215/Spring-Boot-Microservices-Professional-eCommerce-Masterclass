5ebed4ac269bd9fa6f69e05cdb4d7bc0719beeba
dominhit215@gmail.com <dominhit215@gmail.com>
2025-08-09 18:51:50 +0700

    cards: crud api

--- cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
+++ cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.eazybytes.entity;
+package com.eazybytes.cards.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
--- cards/src/main/java/com/eazybytes/cards/entity/Cards.java
+++ cards/src/main/java/com/eazybytes/cards/entity/Cards.java
@@ -1,4 +1,4 @@
-package com.eazybytes.entity;
+package com.eazybytes.cards.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
